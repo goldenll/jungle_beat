8735d20127ac934747d8c1adaed27f2fd11256ca
Lauren Golden <llgolden@gmail.com>
2023-03-25 15:32:10 -0600

    Add linked list spec file after Iteration 1 is complete

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,62 +1,37 @@
-require 'rspec'
 require "./lib/linked_list"
 require "./lib/node"
 
 RSpec.describe LinkedList do
   it "exists" do
     list = LinkedList.new
-
     expect(list).to be_an_instance_of(LinkedList)
   end
 
   it "does not initially contain a head" do
     list = LinkedList.new
-
     expect(list.head).to eq(nil)
   end
 
   it "can add nodes" do
     list = LinkedList.new
-
     list.append("doop")
-
     expect(list.head.data).to eq("doop") 
     expect(list.head.next_node).to eq(nil)
-    # expect(list.count).to eq(1)
+    expect(list.count).to eq(1)
+    expect(list.to_string).to eq("doop")
   end
 
-  xit "can add other nodes" do
+  it "can add multiple nodes" do
     list = LinkedList.new
+    expect(list.head).to eq(nil)
+
+    list.append("doop")
+    expect(list.head.data).to eq("doop") 
+    expect(list.head.next_node).to eq(nil)
 
     list.append("deep")
-
-    expect(list.head.data).to eq("deep") 
+    expect(list.head.next_node.data).to eq("deep") 
+    expect(list.count).to eq(2) 
+    expect(list.to_string).to eq("doop deep")
   end
-
-
-
-
 end
-
-
-
-
-
-# list.count
-# # => 1
-
-# list.to_string
-# # => "doop"
-
-
-  # it "" do
-  #   list = LinkedList.new
-
-  #   expect().to eq()
-  # end
-
-  # it "" do
-  #   list = LinkedList.new
-
-  #   expect().to eq()
-  # end
