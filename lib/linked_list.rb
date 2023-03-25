2f775e2375dc25e44294b817a3f19c8ea38fd7be
Lauren Golden <llgolden@gmail.com>
2023-03-25 15:30:37 -0600

    Add linked list file after Iteration 1 is complete

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,9 +1,8 @@
 class LinkedList
-  attr_reader :head, :next_node
+  attr_reader :head 
 
   def initialize 
     @head = nil
-    @next_node = nil
     @nodes_count = 0
   end
 
@@ -11,29 +10,32 @@ class LinkedList
     if @head == nil
       @head = Node.new(data)
       @nodes_count += 1
-    else 
-      current_node = @head
-      while(current_node.next_node)
-        current_node = current_node.next_node
-      end
-      current_node.next_node = Node.new(data)
+    else
+      make_new_node(data)
       @nodes_count += 1
     end
+  end
+
+  def make_new_node(data)
+    current_node = @head
+    until current_node.next_node.nil?
+      current_node = current_node.next_node
+    end
+    current_node.next_node = Node.new(data)
   end
 
   def count
     @nodes_count
   end
 
-
-
-
-  # def to_string
-  # end
-
-
+  def to_string
+    string = ""
+    string << @head.data
+    current_node = @head.next_node
+    until current_node == nil
+      string << " " + current_node.data
+      current_node = current_node.next_node
+    end
+    string
+  end  
 end
-
-# append - adds a new piece of data (data can really be anything) to the list
-# count - tells us how many things are in the list
-# to_string - generates a string of all the elements in the list, separated by spaces
