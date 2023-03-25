b510dc55525c0d5ad6233c6c0281cc50e59287b7
Lauren Golden <llgolden@gmail.com>
2023-03-25 15:44:22 -0600

    Update file for Iteration 2 start

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -37,5 +37,20 @@ class LinkedList
       current_node = current_node.next_node
     end
     string
-  end  
+  end
+  
+  def prepend(data)
+
+    
+  end
+
+# prepend will add nodes to the beginning of the list.
+
+
 end
+
+
+
+
+
+# insert will insert one or more elements at a given position in the list. It takes two parameters, the first one is the position at which to insert nodes, the second parameter is the string of data to be inserted.
