a679c020e6d1215a6a0798b4ac06a6b2315d8ed1
Lauren Golden <llgolden@gmail.com>
2023-03-26 13:35:03 -0600

    Update file after meeting with student support

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,5 +1,29 @@
-class JungleBeat
-  # def initialize()
+require "./lib/linked_list"
+class JungleBeat       
+  attr_reader :list 
+
+  def initialize
+    @list = list
+  end
+"Update file after meeting with student support"
+  def list
+    list = LinkedList.new
+  end
+
+  def append(data)
+    broken = data.split
+    broken.each do |broke|
+    require 'pry'; binding.pry
+
+      list.append(broke)
+    end
+  end
+
+ 
+
+
+
   
-  # end
-end
+end #final
+
+# With JungleBeat as an extra layer, it can take care of properly formatting the data (eg: splitting the string) before passing it down to the LinkedList. This implementation results in three nodes appended to the list if we pass the string “deep bop dop” to JungleBeat#append.
