8bfdaf740bd80b0597523739999d22246a23027d
Lauren Golden <llgolden@gmail.com>
2023-03-26 17:13:38 -0600

    Add count method

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -3,7 +3,7 @@ class JungleBeat
 
   def initialize
     @list = new_list
-    # @datum_count = 0
+    @datum_count = 0
   end
 
   def new_list
@@ -15,24 +15,14 @@ class JungleBeat
     broken_data.each do |datum|
       @list.append(datum)
     end
+    nums = broken_data.size 
+    @datum_count += nums
     data
   end
 
-
-#  #need a counter, linked list counter will not work
-  # def count
-  #   @datum_count
-  #   list.each do |datum|
-  #     @datum_count += 1
-  #   end
-  # end
-
-
-
-
-
-
-  
+  def count
+    @datum_count
+  end
 end #final
 
 # With JungleBeat as an extra layer, it can take care of properly formatting the data (eg: splitting the string) before passing it down to the LinkedList. This implementation results in three nodes appended to the list if we pass the string “deep bop dop” to JungleBeat#append.
