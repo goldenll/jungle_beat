c254569438aa9ac04b096d4ee90072ec2f95f2bb
Lauren Golden <llgolden@gmail.com>
2023-03-26 17:15:30 -0600

    Update tests

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -3,31 +3,13 @@ require "./lib/linked_list"
 require "./lib/node"
 
 RSpec.describe JungleBeat do
-  it "exists" do
+  it "can split a string" do
     jb = JungleBeat.new
-    expect(jb).to be_an_instance_of(JungleBeat)
-
-    jb.append("deep doo ditt")
+    expect(jb.list.head).to eq(nil)
+    expect(jb.append("deep doo ditt")).to eq("deep doo ditt") 
     expect(jb.list.head.data).to eq("deep") 
+    expect(jb.list.head.next_node.data).to eq("doo")
+    expect(jb.append("woo hoo shu")).to eq("woo hoo shu") 
+    expect(jb.count).to eq(6) 
   end
-  
-
-
-# jb.append("deep doo ditt")
-# => "deep doo ditt"
-
-# jb.list.head.data
-# => "deep"
-
-# jb.list.head.next_node.data
-# => "doo"
-
-# jb.append("woo hoo shu")
-# => "woo hoo shu"
-
-# jb.count
-# 6
-
-
-
 end #final
