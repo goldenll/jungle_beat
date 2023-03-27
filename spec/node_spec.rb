5fc706de3db97527556a2e9e121e9d829b687cf3
Lauren Golden <llgolden@gmail.com>
2023-03-27 11:26:42 -0600

    refactor: updating tests to match iteration pattern

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,11 +1,6 @@
 require "./lib/node" 
 
 RSpec.describe Node do
-  it "exists" do
-    node = Node.new("plop")
-    expect(node).to be_an_instance_of(Node)
-  end
-
   it "contains data" do
     node = Node.new("plop")
     expect(node.data).to eq("plop")
