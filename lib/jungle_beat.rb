ea95cf640bff9ac0b30019cdaca14b40701ebcff
Lauren Golden <llgolden@gmail.com>
2023-03-26 21:04:54 -0600

    Complete iteration 3 first draft

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -23,6 +23,8 @@ class JungleBeat
   def count
     @datum_count
   end
-end #final
 
-# With JungleBeat as an extra layer, it can take care of properly formatting the data (eg: splitting the string) before passing it down to the LinkedList. This implementation results in three nodes appended to the list if we pass the string “deep bop dop” to JungleBeat#append.
+  def play(beats) 
+    `say -r 500 -v Boing #{beats}`
+  end
+end 
